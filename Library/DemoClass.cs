2175a2fd7bf3afadd4b0c21f0a3c522a039ff23e
Matt Lacey <matt@mrlacey.co.uk>
2021-03-23 11:10:19 +0000

    add sample shared library

---
+++ Library/DemoClass.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections.Generic;
+
+namespace MyDemoSharedLibrary
+{
+    public class DemoClass
+    {
+        public IEnumerable<string> GetSomeSampleData()
+        {
+            yield return "Hello";
+            yield return "my";
+            yield return "friends";
+            yield return "enjoy";
+            yield return "this";
+            yield return "text";
+        }
+    }
+}
